f681ed7e3cbe2e5e72e814ccf92808f43860eb35
Synx001 <fromrhythmgame@gmail.com>
2026-02-11 22:22:15 -0800

    update 11/2/2026

---
+++ Home.js
@@ -0,0 +1,9 @@
+const vnHistory = document.querySelector('.vnhistory');
+    vnHistory.addEventListener('click', function() {
+        window.location.href = "VietnamHistory.html";
+    })
+
+const europeHistory = document.querySelector('.europehistory');
+    europeHistory.addEventListener('click', function() {
+        window.location.href = "EuropeHistory.html";
+    })
--- Register.js
+++ Register.js
@@ -1,4 +1,16 @@
 const submit = document.querySelector('.submit')
     submit.addEventListener('click', function() {
+        const email = document.querySelector('.Email_input').value;
+        const password = document.querySelector('.Password_input').value;
+            if (email === "" || password === "") {
+                return alert("Hãy điền thông tin vào trong input")
+            }
+        const user = [
+            {
+                email: email,
+                password: password,
+            }
+        ]
+        localStorage.setItem("user", JSON.stringify(user))
         window.location.href = "Greatings.html";
     })
