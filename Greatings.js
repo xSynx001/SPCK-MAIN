a4424ebb6b7395ee134e2307056288fa21b7cfa9
xSynx001 <fromrhythmgame@gmail.com>
2026-02-14 15:13:30 +0700

    updated

--- Greatings.js
+++ Greatings.js
@@ -1,4 +1,15 @@
 const submit = document.querySelector('.submit');
     submit.addEventListener('click', function() {
-        window.location.href = 'Home.html';
+        const a = document.querySelector(".Email_input").value;
+        const b = document.querySelector(".Password_input").value;
+             if (a === "" || b === "")
+                return alert("Hãy điền đầy đủ thông tin")
+        const storeduser = JSON.parse(localStorage.getItem("user"));
+            if (storeduser && storeduser.email === a && storeduser.password === b) {
+                window.location.href = "Greatings.html";
+                return alert("Email và mật khẩu đúng, login thành công")
+            }
+            else {
+                return alert("Email hoặc mật khẩu không đúng , hãy check lại thông tin")
+            }
     })
